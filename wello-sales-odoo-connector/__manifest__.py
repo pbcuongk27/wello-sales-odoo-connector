1301a838ef4fa1062d122cc823839f78b93550d9
Cuong Pham <c.pham@odysseemobile.com>
2024-01-26 10:57:04 +0700

    Init wello sales odoo connector

---
+++ wello-sales-odoo-connector/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Wello Odoo Connector',
+    'version':'1.0',
+    'sequence': 1,
+    'description':'Wello Odoo Connector',
+    'author':'Wello',
+    'website':'https://wello.solutions/',
+    'license':'LGPL-3',
+    'installable':'False',
+    'images':['static/description/wello_description.gif'],
+    'live_test_url': 'https://portal.wello.solutions/marketplace?connect=odoo',
+}
